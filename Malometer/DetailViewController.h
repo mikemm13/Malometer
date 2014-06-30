5cf902e0c842e64548f8d603de80179ed87706d8
Miguel Martín-Nieto Morales <miguel@contxt.in>
2014-06-30 11:41:52 +0200

    Initial Commit

---
+++ Malometer/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  Malometer
+//
+//  Created by Miguel Martin Nieto on 30/06/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
