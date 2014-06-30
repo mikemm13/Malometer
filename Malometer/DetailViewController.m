f39dfe11ec9d81b78109a182f58fb89f4dabd70b
Miguel Martín-Nieto Morales <miguel@contxt.in>
2014-06-30 12:49:42 +0200

    After creating properties ex5

--- Malometer/DetailViewController.m
+++ Malometer/DetailViewController.m
@@ -8,7 +8,21 @@
 
 #import "DetailViewController.h"
 
+static NSArray *destructionValues;
+static NSArray *motivationValues;
+static NSArray *assessmentValues;
+
 @interface DetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UILabel *assessment;
+@property (weak, nonatomic) IBOutlet UILabel *destructionPowerValue;
+@property (weak, nonatomic) IBOutlet UILabel *motivationValue;
+@property (weak, nonatomic) IBOutlet UIStepper *destructionPowerStepper;
+
+@property (weak, nonatomic) IBOutlet UIStepper *motivationStepper;
+
+
 - (void)configureView;
 @end
 
@@ -30,9 +44,7 @@
 {
     // Update the user interface for the detail item.
 
-    if (self.detailItem) {
-        self.detailDescriptionLabel.text = [[self.detailItem valueForKey:@"timeStamp"] description];
-    }
+    
 }
 
 - (void)viewDidLoad
@@ -40,6 +52,18 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [self configureView];
+    destructionValues = @[@"None",@"Little girl", @"Neutral", @"Killer", @"Massive destruction"];
+    motivationValues = @[@"None", @"Bored", @"OK", @"Motivated", @"Very motivated"];
+    assessmentValues = @[@"No Way", @"Better Not", @"Maybe", @"Yes", @"A Must"];
+    
+    self.assessment = assessmentValues[0];
+    self.destructionPowerValue = destructionValues[0];
+    self.motivationValue = motivationValues[0];
+    self.motivationStepper.value = 0;
+    self.destructionPowerStepper.value = 0;
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
--- Malometer/MasterViewController.m
+++ Malometer/MasterViewController.m
@@ -25,10 +25,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+//    self.navigationItem.leftBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,25 +34,25 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)insertNewObject:(id)sender
-{
-    NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
-    NSEntityDescription *entity = [[self.fetchedResultsController fetchRequest] entity];
-    NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
-    
-    // If appropriate, configure the new managed object.
-    // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-    [newManagedObject setValue:[NSDate date] forKey:@"timeStamp"];
-    
-    // Save the context.
-    NSError *error = nil;
-    if (![context save:&error]) {
-         // Replace this implementation with code to handle the error appropriately.
-         // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
-    }
-}
+//- (void)insertNewObject:(id)sender
+//{
+//    NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
+//    NSEntityDescription *entity = [[self.fetchedResultsController fetchRequest] entity];
+//    NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
+//    
+//    // If appropriate, configure the new managed object.
+//    // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
+//    [newManagedObject setValue:[NSDate date] forKey:@"timeStamp"];
+//    
+//    // Save the context.
+//    NSError *error = nil;
+//    if (![context save:&error]) {
+//         // Replace this implementation with code to handle the error appropriately.
+//         // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+//        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+//        abort();
+//    }
+//}
 
 #pragma mark - Table View
 
