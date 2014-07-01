767d2293092d2292cbf38d42029c3d2d5bf435f4
Miguel Martín-Nieto Morales <miguel@contxt.in>
2014-07-01 11:11:19 +0200

    Create relationships

--- Malometer/Agent.h
+++ Malometer/Agent.h
@@ -2,7 +2,7 @@
 //  Agent.h
 //  Malometer
 //
-//  Created by Jorge D. Ortiz Fuentes on 12/06/14.
+//  Created by Miguel Martin Nieto on 01/07/14.
 //  Copyright (c) 2014 PoWWaU. All rights reserved.
 //
 
@@ -17,5 +17,7 @@
 @property (nonatomic, retain) NSNumber * motivation;
 @property (nonatomic, retain) NSString * name;
 @property (nonatomic, retain) NSString * pictureUUID;
+@property (nonatomic, retain) NSManagedObject *category;
+@property (nonatomic, retain) NSManagedObject *domains;
 
 @end
--- Malometer/Agent.m
+++ Malometer/Agent.m
@@ -2,7 +2,7 @@
 //  Agent.m
 //  Malometer
 //
-//  Created by Jorge D. Ortiz Fuentes on 12/06/14.
+//  Created by Miguel Martin Nieto on 01/07/14.
 //  Copyright (c) 2014 PoWWaU. All rights reserved.
 //
 
@@ -16,5 +16,7 @@
 @dynamic motivation;
 @dynamic name;
 @dynamic pictureUUID;
+@dynamic category;
+@dynamic domains;
 
 @end
